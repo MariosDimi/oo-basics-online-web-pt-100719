59e319776e09e2b87574b01e628b483391e7e232
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-16 15:55:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material
+  attr_reader :condition
+  
+  def initialize(condition)
+    @condition = condition
+  end
+  
+  def shoe_repair
+    puts "The shoe is been repaired, good as new!"
+  end
+end
